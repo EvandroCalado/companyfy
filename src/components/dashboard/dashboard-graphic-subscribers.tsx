67cb8decb094d2f7b6a5651f7a930e0d80aab7e6
Evandro Calado <evandrocalado07@gmail.com>
2025-03-26 08:52:39 -0300

    feat: enhance DashboardGraphicSubscribers component with new layout and badge for visitor statistics

--- src/components/dashboard/dashboard-graphic-subscribers.tsx
+++ src/components/dashboard/dashboard-graphic-subscribers.tsx
@@ -3,6 +3,7 @@
 import { TrendingUp } from 'lucide-react';
 import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 
+import { Badge } from '../ui/badge';
 import {
   Card,
   CardContent,
@@ -40,66 +41,79 @@ const chartConfig = {
 
 export const DashboardGraphicSubscribers = () => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Area Chart - Stacked</CardTitle>
-        <CardDescription>
-          Showing total visitors for the last 6 months
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <AreaChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              left: 12,
-              right: 12,
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey='month'
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator='dot' />}
-            />
-            <Area
-              dataKey='mobile'
-              type='natural'
-              fill='var(--color-mobile)'
-              fillOpacity={0.4}
-              stroke='var(--color-mobile)'
-              stackId='a'
-            />
-            <Area
-              dataKey='desktop'
-              type='natural'
-              fill='var(--color-desktop)'
-              fillOpacity={0.4}
-              stroke='var(--color-desktop)'
-              stackId='a'
-            />
-          </AreaChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter>
-        <div className='flex w-full items-start gap-2 text-sm'>
-          <div className='grid gap-2'>
-            <div className='flex items-center gap-2 leading-none font-medium'>
-              Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
-            </div>
-            <div className='text-muted-foreground flex items-center gap-2 leading-none'>
-              January - June 2024
+    <div className='space-y-2'>
+      <div className='text-2xl font-semibold'>24.479</div>
+      <div className='flex items-center gap-2'>
+        <Badge className='rounded-full'>
+          <span>12.1%</span>
+          <TrendingUp strokeWidth={1} />
+        </Badge>
+
+        <p className='text-muted-foreground text-sm'>+432 increased</p>
+      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Area Chart - Stacked</CardTitle>
+          <CardDescription>
+            Showing total visitors for the last 6 months
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey='month'
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent indicator='dot' />}
+              />
+              <Area
+                dataKey='mobile'
+                type='natural'
+                fill='var(--color-mobile)'
+                fillOpacity={0.4}
+                stroke='var(--color-mobile)'
+                stackId='a'
+              />
+              <Area
+                dataKey='desktop'
+                type='natural'
+                fill='var(--color-desktop)'
+                fillOpacity={0.4}
+                stroke='var(--color-desktop)'
+                stackId='a'
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter>
+          <div className='flex w-full items-start gap-2 text-sm'>
+            <div className='grid gap-2'>
+              <div className='flex items-center gap-2 leading-none font-medium'>
+                Trending up by 5.2% this month{' '}
+                <TrendingUp className='h-4 w-4' />
+              </div>
+              <div className='text-muted-foreground flex items-center gap-2 leading-none'>
+                January - June 2024
+              </div>
             </div>
           </div>
-        </div>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
